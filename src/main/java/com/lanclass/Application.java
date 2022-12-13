3ceef86cc24810a2dc8d45abcf6c3d28e17435c2
蓝绍恒 <1527715476@qq.com>
2022-12-13 22:10:04 +0800

    第一次提交

---
+++ src/main/java/com/lanclass/Application.java
@@ -0,0 +1,26 @@
+package com.lanclass;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    @Value("${server.port}")
+    private int serverPort;
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args)  {
+        LOGGER.info("--------------------启动成功------------------------");
+        LOGGER.info("--------------------访问端口{}---------------------", serverPort);
+    }
+}
