33ef2f10bb7ab4a3a45a668bc74f8b704637e6ed
zylan <1527715476@qq.com>
2022-12-15 10:36:51 +0800

    整合mybatis

--- src/main/java/com/lanclass/Application.java
+++ src/main/java/com/lanclass/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.lanclass.mapper")
+@MapperScan("com.lanclass.dao")
 @SpringBootApplication
 public class Application implements CommandLineRunner {
     private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
