2be8db2a733866c44d7481a502142291f81326a9
蓝绍恒 <1527715476@qq.com>
2022-12-16 00:07:04 +0800

    数据库切换

--- src/main/java/com/lanclass/Application.java
+++ src/main/java/com/lanclass/Application.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @MapperScan("com.lanclass.dao")
 @SpringBootApplication
 public class Application implements CommandLineRunner {
